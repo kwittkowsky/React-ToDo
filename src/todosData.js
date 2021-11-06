b8e10293dda57e251e1d024f45ec949e17ac5ee5
kwittkowsky <katharinawittkowsky@gmail.com>
2021-11-06 11:12:48 +0100

    added todosData.js

---
+++ src/todosData.js
@@ -0,0 +1,22 @@
+const todosData = [
+  {
+    id: 1,
+    text: 'Müll rausbringen',
+    completed: false,
+  },
+  {
+    id: 2,
+    text: 'Geschirrspüler ausräumen',
+    completed: true,
+  },
+  {
+    id: 3,
+    text: 'Wäsche zusammenlegen',
+    completed: false,
+  },
+  {
+    id: 4,
+    text: 'Lebensmittel einkaufen',
+    completed: true,
+  },
+];
