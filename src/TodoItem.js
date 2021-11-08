90537525786ce5eb0019ed6dadc6798b688e5caa
Katharina Wittkowsky <katharina.wittkowsky@ffwagency.com>
2021-11-08 12:38:40 +0100

    Conditional rendering example

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,12 +3,20 @@ import React from 'react';
 /**
  * Added probs
  * get from parent as object (like probs.item.id)
+ * added conditional rendering
  */
 export default function TodoItem(probs) {
   return (
     <div className="todo-item">
       <input type="checkbox" checked={probs.item.completed} />
-      <p>{probs.item.text}</p>
+      <p>
+        { probs.item.completed ? (
+          <del>{probs.item.text}</del>
+          ) : (
+            probs.item.text
+          )
+        }
+      </p>
     </div>
   );
 }
